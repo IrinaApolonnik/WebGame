9330a6e333ee14c2600adeede89626a7c3a192c7
IrinaApolonnik <iaapolonnik@edu.hse.ru>
2023-03-21 12:06:08 +0300

    kjk

--- java/index.js
+++ java/index.js
@@ -1,4 +1,20 @@
 $(document).ready(function(){
+	$("body").scroll(function() {
+		$("knopka1").css("top", "50%");
+	});
+	$("body").mousemove(function(event) {
+		let eye = $(".eye");
+		let x = (eye.offset().left) + (eye.width() / 2);
+		let y = (eye.offset().top) + (eye.height() / 2);
+		let rad = Math.atan2(event.pageX - x, event.pageY - y);
+		let rot = (rad * (180 / Math.PI) * -1) + 180;
+		eye.css({
+			'-webkit-transform': 'rotate(' + rot + 'deg)',
+			'-moz-transform': 'rotate(' + rot + 'deg)',
+			'-ms-transform': 'rotate(' + rot + 'deg)',
+			'transform': 'rotate(' + rot + 'deg)'
+		});
+	});
 	$(".knopka1").click(function(){
 		$("body").css("background-color", "#FFF3E8" );
 		$(".knop1").toggleClass("knop");
@@ -93,10 +109,10 @@ $(document).ready(function(){
 	});
 
 	$(".help2, .help1, .help3").click(function(){
-		$(".tablet").toggleClass("show");
+		$(".tablet").toggleClass("show-tablet");
 		$(".tablet").toggleClass("hide");
 		setTimeout(function(){
-			$(".tablet").toggleClass("show");
+			$(".tablet").toggleClass("show-tablet");
 		}, 1200);
 	});
 
@@ -110,14 +126,27 @@ $(document).ready(function(){
 
  // взаимодействие с кошкой удачи
 	$(".nekocat").click(function(){
-		$(".coinx").css("animation-play-state", "paused" );
+		let myArray = [
+  	"10%",
+  	"20%",
+  	"30%",
+		"40%",
+		"50%",
+		"60%",
+		"70%",
+		"80%"
+		];
+		let randomItem = myArray[Math.floor(Math.random()*myArray.length)];
+
+		// $(".coinx").css("animation-play-state", "paused" );
+		$(".yen").css("left", randomItem );
 		$(".yen").toggleClass("coiny" );
 		$(".nekocat img:nth-of-type(2)").css("display", "none" );
 		$(".nekocat img:nth-of-type(3)").css("display", "block" );
 		setTimeout(function(){
 			$(".nekocat img:nth-of-type(3)").css("display", "none" );
 			$(".nekocat img:nth-of-type(2)").css("display", "block" );
-			$(".coinx").css("animation-play-state", "running");
+			// $(".coinx").css("animation-play-state", "running");
 			$(".yen").toggleClass("coiny" );
 		}, 1200);
 	});
@@ -146,5 +175,24 @@ $(document).ready(function(){
 	// падающие цветы сакуры
 	$(".sakura img").click(function(){
 		$(this).addClass("fall");
+	});
+
+	$(function(){
+		let count = 0;
+        $(".sakura img:nth-of-type(1), .sakura img:nth-of-type(4), .sakura img:nth-of-type(7), .sakura img:nth-of-type(21)").click(function () {
+          count += 1;
+          if (count == 1) {
+            $('#count_sak').html("1/4");
+          }
+					else if (count == 2) {
+						$('#count_sak').html("2/4");
+					}
+					else if (count == 3) {
+						$('#count_sak').html("3/4");
+					}
+					else {
+						$('#count_sak').html("4/4");
+					}
+        });
 	});
 });
